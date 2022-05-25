03b02f672e450de88de3ece00c3587894685b4fe
shiprakri <100258511+shiprakri@users.noreply.github.com>
2022-05-25 16:21:27 +0530

    Update auth.js

--- Server/middleware/auth.js
+++ Server/middleware/auth.js
@@ -6,7 +6,7 @@ const auth = async (req, res, next) => {
   try {
   
     const token = req.headers.authorization.split(" ")[1];
-    const isCustomAuth = token.length < 500;
+    
   
     let decodedData;
 
@@ -14,10 +14,7 @@ const auth = async (req, res, next) => {
       decodedData = jwt.verify(token, secret);
 
       req.userId = decodedData?.id;
-    } else {
-      decodedData = jwt.decode(token);
-
-      req.userId = decodedData?.sub;
+    } 
     }    
 
     next();
